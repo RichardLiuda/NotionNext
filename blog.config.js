02cd5d89c9945640a54402a5e9ec2c56a1e962c3
Richard Liu <2645345468@qq.com>
2024-10-24 01:28:20 +0800

    music

--- blog.config.js
+++ blog.config.js
@@ -327,8 +327,8 @@ const BLOG = {
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || true, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
-  MUSIC_PLAYER_METING_ID://music.163.com/playlist?id=6815445130&uct2=U2FsdGVkX18Dm9b+CrZH031W4ZoVWURJEJPoJx4m62I=
-    process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_ID || '681544', // 对应歌单的 id
+  MUSIC_PLAYER_METING_ID://music.163.com/playlist?id=873654427&uct2=U2FsdGVkX18arPi9lIXDY2+lv8ISJRm70fz6KCqcuBA=
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_ID || '873654427', // 对应歌单的 id
   MUSIC_PLAYER_METING_LRC_TYPE:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_LRC_TYPE || '1', // 可选值： 3 | 1 | 0（0：禁用 lrc 歌词，1：lrc 格式的字符串，3：lrc 文件 url）
 
